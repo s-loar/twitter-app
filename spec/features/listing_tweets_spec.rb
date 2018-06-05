7f028beaa3217500a39966c076939fc3cfc5864a
Steven Loar <sjl7909@gmail.com>
2018-06-05 15:20:30 -0400

    Add no tweets to list and tests

--- spec/features/listing_tweets_spec.rb
+++ spec/features/listing_tweets_spec.rb
@@ -14,4 +14,18 @@ RSpec.feature "List Tweets" do
     expect(page).to have_content(@tweet2.body)
     expect(page).to have_link(@tweet2.body)
   end
+
+  scenario "No tweets to list" do
+    Tweet.delete_all
+    visit "/"
+
+    expect(page).not_to have_content(@tweet1.body)
+    expect(page).not_to have_link(@tweet1.body)
+    expect(page).not_to have_content(@tweet2.body)
+    expect(page).not_to have_link(@tweet2.body)
+
+    within("h1#no-tweets") do
+      expect(page).to have_content("No Tweets Posted")
+    end
+  end
 end
