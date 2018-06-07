90336719db2b77059c408f953eb43fdd183ba785
Steven Loar <sjl7909@gmail.com>
2018-06-07 09:11:22 -0400

    Restrict edit and delete to signed in owners

--- spec/features/show_tweet_spec.rb
+++ spec/features/show_tweet_spec.rb
@@ -4,14 +4,39 @@ RSpec.feature "Show a Tweet" do
 
   before do
     @jake = User.create!(email: "jake@example.com", password: "password")
+    @sally = User.create!(email: "sally@example.com", password: "password")
     @tweet = Tweet.create(body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", user: @jake)
   end
 
-  scenario "to show a tweet" do
+  scenario "hide the edit and delete from a not signed in user" do
     visit '/'
     click_link @tweet.body
 
     expect(page).to have_content(@tweet.body)
     expect(page.current_path).to eq(tweet_path(@tweet))
+    expect(page).not_to have_link("Edit Tweet")
+    expect(page).not_to have_link("Delete Tweet")
+  end
+
+  scenario "hide the edit and delete from a signed in, non-owner user" do
+    login_as(@sally)
+    visit '/'
+    click_link @tweet.body
+
+    expect(page).to have_content(@tweet.body)
+    expect(page.current_path).to eq(tweet_path(@tweet))
+    expect(page).not_to have_link("Edit Tweet")
+    expect(page).not_to have_link("Delete Tweet")
+  end
+
+  scenario "show the edit and delete to a signed in, owner user" do
+    login_as(@jake)
+    visit '/'
+    click_link @tweet.body
+
+    expect(page).to have_content(@tweet.body)
+    expect(page.current_path).to eq(tweet_path(@tweet))
+    expect(page).to have_link("Edit Tweet")
+    expect(page).to have_link("Delete Tweet")
   end
 end
